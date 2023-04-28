78fd659b9cc28db4feeb4b54a6b60532189115c4
Ashwash <latar14@bk.ru>
2023-04-28 15:44:10 +0300

    header is added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,60 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai/index';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai/index";
+import { DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span
+} from "./HeaderStyles";
 
-const Header = () =>  (
+const Header = () => (
   <Container>
     <Div1>
-      <Link href='/'>
-        <a style={{display: "flex", alignItems: "center", color: "white"}}>
-          <DiCssdeck  size="3rem"/> <span>Portfolio</span>
-            
-        </a>
+      <Link
+        style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px"  }}
+        href="/"
+      >
+        <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/latar14">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/latar14">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com/ashwash__">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
